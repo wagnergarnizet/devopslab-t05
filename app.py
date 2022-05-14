e7ddbb245fa5098751cbeda66415ff49bda63c7d
Wagner Garnizet da Silva <61944236+wagnergarnizet@users.noreply.github.com>
2022-05-14 12:23:06 -0300

    teste3

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def pagina_inicial():
-    return "Hello World - Gabriel, o Palmeiras não tem mundial"
+    return "Hello World"
 
 if __name__ == '__main__':
     app.run()
